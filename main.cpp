0840fe6871a2b5862f4d287c43ddca2c41edb284
Evan Wilt <ev.wilt@gmail.com>
2019-06-21 22:23:07 -0400

    Adding chapter 4 changes

--- main.cpp
+++ main.cpp
@@ -5,22 +5,36 @@
 #include "Vector3.h"
 #include "Ray.h"
 
+bool hit_sphere(const Vector3& center, float radius, const Ray& ray) {
+    Vector3 diff = ray.get_origin() - center;
+    float a = dot_product(ray.get_direction(), ray.get_direction());
+    float b = 2.0 * dot_product(diff, ray.get_direction());
+    float c = dot_product(diff, diff) - pow(radius, 2);
+    float discriminant = pow(b, 2) - 4 * a * c;
+    return discriminant > 0;
+}
+
 Vector3 color(const Ray& ray) {
+    if (hit_sphere(Vector3(0.0, 0.0, -1.0), 0.5, ray)) {
+        return Vector3(1.0, 0.0, 0.0);
+    }
     Vector3 unitDirection = unit_vector(ray.get_direction());
     float t = 0.5 * (unitDirection.y() + 1.0);
     return (1.0 - t) * Vector3(1.0, 1.0, 1.0) + t * Vector3(0.5, 0.7, 1.0);
 }
 
 int main() {
-    int width = 200;
-    int height = 100;
+    int width = 600;
+    int height = 300;
     Vector3 lowerLeft(-2.0, -1.0, -1.0);
     Vector3 horizontal(4.0, 0.0, 0.0);
     Vector3 vertical(0.0, 2.0, 0.0);
     Vector3 origin(0.0, 0.0, 0.0);
     std::vector<unsigned char> rgb;
+
     for (int y = height - 1; y >= 0; --y) {
         for (int x = 0; x < width; ++x) {
+
             float u = float(x) / float(width);
             float v = float(y) / float(height);
 
